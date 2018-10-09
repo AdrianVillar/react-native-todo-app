c3b950121bd5d1808a9142397199428105d111ec
Siya Nkosi <siya.nkosi@fnb.co.za>
2018-10-09 20:50:41 +0200

    android animation

--- components/TaskRow/Render.android.js
+++ components/TaskRow/Render.android.js
@@ -1,17 +1,49 @@
 import React from 'react';
-import {View, Text, TouchableHighlight} from 'react-native';
+import {
+  View, Text, TouchableHighlight, Image, StyleSheet, Animated
+} from 'react-native';
+
 
 export default function render(styles) {
+  const doneAnimation = new Animated.ValueXY();
+
+  const localStyle = StyleSheet.create({
+    doneButton:{
+      borderRadius: 5,
+      padding: 5,
+    },
+    row:{
+      transform: doneAnimation.getTranslateTransform(),
+    }
+  });
+
+  function animatedPress(){
+    Animated.spring(doneAnimation,{
+      tension: 2,
+      friction: 3,
+      toValue:{
+        x: 0,
+        y: -500,
+      }
+    }).start();
+
+    setTimeout(()=>{
+      this.onDonePressed();
+    },500);
+  }
+
   return (
-    <View style={styles.container}>
+    <Animated.View style={[styles.container, localStyle.row]}>
       <Text style={styles.label}>{this.props.todo.task}</Text>
 
       <TouchableHighlight
-        onPress={this.onDonePressed.bind(this)}
-        style={styles.doneButton}
-      >
-        <Text style={[styles.label, styles.doneButtonLabel]}>Done</Text>
+        underlayColor="#ddd"
+        onPress={animatedPress.bind(this)}
+        style={localStyle.doneButton} >
+
+        <Image
+          source={require('../../images/done.png')} />
       </TouchableHighlight>
-    </View>
+    </Animated.View>
   );
 }
--- components/TaskRow/Render.ios.js
+++ components/TaskRow/Render.ios.js
@@ -1,18 +1,32 @@
-import React from 'react';
+import React from "react";
 
-import {View, Text, TouchableHighlight} from 'react-native';
+import { View, Text, TouchableHighlight } from "react-native";
+import SwipeOut from "react-native-swipeout";
 
 export default function render(styles) {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.label}>{this.props.todo.task}</Text>
+  const buttons = [
+    {
+      text: "Done",
+      backgourndColor: "#05A5D1",
+      underlayColor: "#273539",
+      onPress: this.onDonePressed.bin(this)
+    }
+  ];
 
-      <TouchableHighlight
-        onPress={this.onDonePressed.bind(this)}
-        style={styles.doneButton}
-      >
-        <Text style={[styles.label, styles.doneButtonLabel]}>Done</Text>
-      </TouchableHighlight>
-    </View>
+  return (
+    <SwipeOut
+      backgroundColor="#fff"
+      right={buttons}>
+      <View style={styles.container}>
+        <Text style={styles.label}>{this.props.todo.task}</Text>
+
+        <TouchableHighlight
+          onPress={this.onDonePressed.bind(this)}
+          style={styles.doneButton}
+        >
+          <Text style={[styles.label, styles.doneButtonLabel]}>Done</Text>
+        </TouchableHighlight>
+      </View>
+    </SwipeOut>
   );
 }
